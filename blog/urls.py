84a04752e57f2b346b3bf6633c1adffbce0c1c83
MwirigiMuriithi <learnerviner@gmail.com>
2024-08-01 14:02:01 +0300

    changed model name from post to blog and updated views, serializers to use blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path
 from .views import (
-    PostListView, PostDetailView, MyPostListView, PostCreateView, PostUpdateView, PostDeleteView,
+    BlogListView, BlogDetailView, MyBlogListView, BlogCreateView, BlogUpdateView, BlogDeleteView,
     CommentListView, CommentCreateView, CommentDeleteView, AddLikeView, RemoveLikeView
 )
 
 urlpatterns = [
-    path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/my/', MyPostListView.as_view(), name='my-posts'),
-    path('posts/create/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('posts/<int:post_pk>/comments/', CommentListView.as_view(), name='comment-list'),
-    path('posts/<int:post_pk>/comments/create/', CommentCreateView.as_view(), name='comment-create'),
-    path('posts/comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
-    path('posts/<int:pk>/like/', AddLikeView.as_view(), name='add-like'),
-    path('posts/<int:pk>/unlike/', RemoveLikeView.as_view(), name='remove-like'),
+    path('blogs/', BlogListView.as_view(), name='blog-list'),
+    path('blogs/<int:pk>/', BlogDetailView.as_view(), name='blog-detail'),
+    path('blogs/my/', MyBlogListView.as_view(), name='my-blogs'),
+    path('blogs/create/', BlogCreateView.as_view(), name='blog-create'),
+    path('blogs/<int:pk>/update/', BlogUpdateView.as_view(), name='blog-update'),
+    path('blogs/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog-delete'),
+    path('blogs/<int:blog_pk>/comments/', CommentListView.as_view(), name='comment-list'),
+    path('blogs/<int:blog_pk>/comments/create/', CommentCreateView.as_view(), name='comment-create'),
+    path('blogs/comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('blogs/<int:pk>/like/', AddLikeView.as_view(), name='add-like'),
+    path('blogs/<int:pk>/unlike/', RemoveLikeView.as_view(), name='remove-like'),
 ]
